2fbb11f3f0b2396e8b4f27fb3eec6a924d3d8efb
Doug Stewart <hi@dou.gg>
2026-07-09 10:48:36 -0700

    Update packages and configs

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,34 @@
+import path from "node:path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  turbopack: {
+    resolveAlias: {
+      "@/assets": path.join(__dirname, "src/assets"),
+    },
+    rules: {
+      "*.svg": {
+        loaders: [
+          {
+            loader: "@svgr/webpack",
+            options: {
+              titleProp: true,
+              svgoConfig: {
+                plugins: [
+                  {
+                    name: "preset-default",
+                    params: { overrides: { removeViewBox: false } },
+                  },
+                ],
+              },
+            },
+          },
+        ],
+        as: "*.js",
+      },
+    },
+  },
 };
 
 export default nextConfig;
