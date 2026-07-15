373b3b8aa76143d7687d110b7dfc9c39c2b7c81b
Doug Stewart <hi@dou.gg>
2026-07-15 14:54:45 -0700

    Add Noto Sans and fix global layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,15 @@
 import type { Metadata } from "next";
+import { Noto_Sans } from "next/font/google";
 import Providers from "@/components/providers/Providers";
+import { NotificationsList } from "@/features/notifications/components/notifications-list/NotificationsList";
 import "@/styles/reset.css";
 import "@/styles/index.css";
-import { NotificationsList } from "@/features/notifications/components/notifications-list/NotificationsList";
+
+const notoSans = Noto_Sans({
+  subsets: ["latin"],
+  variable: "--font-noto-sans",
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Hedge.gg",
@@ -18,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html className={notoSans.variable} lang="en">
       <body>
         <Providers>
           <NotificationsList />
