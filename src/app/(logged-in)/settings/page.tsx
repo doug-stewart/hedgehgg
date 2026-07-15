4a52192e42a5c796d4c8cfcc0345e41dfd7dcac6
Doug Stewart <hi@dou.gg>
2026-07-15 15:16:02 -0700

    WIP commit: This is going to be removed and the functionality will go into a settings dialog and first time setup component

--- src/app/(logged-in)/settings/page.tsx
+++ src/app/(logged-in)/settings/page.tsx
@@ -1,12 +1,32 @@
-import { redirectIfNoSession } from "@/features/auth/helpers/redirectIfNoSession";
-import { Profile } from "@/features/user/routes/Profile";
+import { dehydrate, HydrationBoundary, queryOptions } from "@tanstack/react-query";
+import { redirect } from "next/navigation";
+import { authClient } from "@/features/auth/lib/auth-client";
+import { getProfile } from "@/features/user/api/getProfile";
+// import { ProfileForm } from "@/features/user/components/profile-form/ProfileForm";
+import { getQueryClient } from "@/helpers/queryClient";
 
 export default async function Page() {
-  await redirectIfNoSession();
+  const session = await authClient.getSession();
+  const userId = session.data?.user.id;
+
+  if (!userId) {
+    redirect("/");
+  }
+
+  const profile = queryOptions({
+    queryKey: ["user", userId],
+    queryFn: async () => getProfile(),
+    staleTime: Infinity,
+  });
+
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(profile);
+
   return (
-    <>
+    <HydrationBoundary state={dehydrate(queryClient)}>
       <title>Settings • Hedge.gg</title>
-      <Profile />
-    </>
+      <h1>Settings</h1>
+      {/*<ProfileForm profile={profile} />*/}
+    </HydrationBoundary>
   );
 }
