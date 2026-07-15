9d6ec46a60d941675725b06df03e8b1e1a970001
Doug Stewart <hi@dou.gg>
2026-07-15 15:21:59 -0700

    Remove services imports

--- src/features/user/routes/Profile.tsx
+++ src/features/user/routes/Profile.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { ServiceForm } from "@/features/services/components/service-form/ServiceForm";
-import { ServiceList } from "@/features/services/components/service-list/ServiceList";
 import { ProfileForm } from "../components/profile-form/ProfileForm";
 import { useProfile } from "../hooks/useProfile";
 
@@ -11,11 +9,7 @@ export const Profile = () => {
   return isSuccess ? (
     <>
       <h1>Settings</h1>
-      <h2>Integrations</h2>
       <ProfileForm profile={profile} />
-      <h2>Services</h2>
-      <ServiceList />
-      <ServiceForm />
     </>
   ) : null;
 };
