e8860b230c4d38afbbc85a3ca6bdf1c9c25d450b
Doug Stewart <hi@dou.gg>
2026-07-20 21:31:13 -0700

    Finish styling pass of user menu with theme colors and improved layout

--- src/features/user/components/user-menu/UserMenu.tsx
+++ src/features/user/components/user-menu/UserMenu.tsx
@@ -3,6 +3,7 @@
 import clsx from "clsx";
 import { useId } from "react";
 import HamburgerMenu from "@/assets/images/hamburger-menu.svg";
+import { Button } from "@/components/button/Button";
 import { useSession } from "@/features/auth/hooks/useSession";
 import { themes } from "../../helpers/themes";
 import { useTheme } from "../../hooks/useTheme";
@@ -17,29 +18,32 @@ export const UserMenu = ({ className }: { className?: string }) => {
 
   return isLoggedIn ? (
     <div className={clsx(styles.wrapper, className)}>
-      <button
+      <Button
         className={styles.toggle}
+        kind="secondary"
         popoverTarget={id}
         popoverTargetAction="toggle"
-        type="button"
       >
         <HamburgerMenu role="presentation" />
-      </button>
+      </Button>
       <dialog className={styles.menu} id={id} popover="auto">
-        <ul>
+        <p className={styles.title}>Select a theme:</p>
+        <ul className={styles.themes}>
           {themes.map((t) => (
-            <button
-              className={clsx(t.value === theme && styles.active)}
-              key={t.value}
-              onClick={() => setTheme.mutate(t.value)}
-              type="button"
-            >
-              <t.icon title={t.label} />
-            </button>
+            <li key={t.value}>
+              <button
+                className={clsx(styles.theme, t.value === theme && styles.active)}
+                onClick={() => setTheme.mutate(t.value)}
+                type="button"
+              >
+                <t.icon title={t.label} />
+              </button>
+            </li>
           ))}
         </ul>
-        <SettingsDialog />
-        <button onClick={logout} type="button">
+        <hr className={styles.divider} />
+        <SettingsDialog buttonClassName={styles.btn} />
+        <button className={styles.btn} onClick={logout} type="button">
           Logout
         </button>
       </dialog>
