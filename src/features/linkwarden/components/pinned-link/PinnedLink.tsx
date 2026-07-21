191b2dc10e3baed449f106681c308e213c873e63
Doug Stewart <hi@dou.gg>
2026-07-20 21:33:41 -0700

    Finish styling pass on pinned links using theme color scheme

--- src/features/linkwarden/components/pinned-link/PinnedLink.tsx
+++ src/features/linkwarden/components/pinned-link/PinnedLink.tsx
@@ -1,8 +1,6 @@
-import clsx from "clsx";
-import Link from "next/link";
 import { useId } from "react";
+import { Button } from "@/components/button/Button";
 import { Tooltip } from "@/components/tooltip/Tooltip";
-import { useTheme } from "@/features/user/hooks/useTheme";
 import { useTooltip } from "@/hooks/useTooltip";
 import { getAbbr } from "../../helpes/getAbbr";
 import { useSelfhstIcons } from "../../hooks/useSelfhstIcons";
@@ -11,51 +9,34 @@ import styles from "./PinnedLink.module.css";
 
 export const PinnedLink = ({ link }: { link: LinkwardenLink }) => {
   const id = useId();
-  const { tooltipRef, showTooltip, hideTooltip, toggleTooltip } = useTooltip();
+  const { tooltipRef, showTooltip, hideTooltip } = useTooltip();
   const { getIconUrl } = useSelfhstIcons();
-  const { display: theme } = useTheme();
 
   const icon = getIconUrl(link.name);
-  const colorIcon = icon?.replace(`-${theme === "light" ? "dark" : "light"}`, "");
   const abbr = getAbbr(link.name);
 
   return (
     <li aria-labelledby={`pin-${id}`} className={styles.item} key={link.id}>
-      <Link
+      <Button
         className={styles.link}
         href={link.url}
         onBlur={hideTooltip}
-        onClick={toggleTooltip}
         onFocus={showTooltip}
         onMouseEnter={showTooltip}
         onMouseLeave={hideTooltip}
       >
         {icon ? (
-          <>
-            {/* biome-ignore lint/performance/noImgElement: because */}
-            <img
-              alt=""
-              className={clsx(styles.icon, styles.monochrome)}
-              height={64}
-              src={icon}
-              width={64}
-            />
-            {/* biome-ignore lint/performance/noImgElement: because */}
-            <img
-              alt=""
-              className={clsx(styles.icon, styles.color)}
-              height={64}
-              src={colorIcon}
-              width={64}
-            />
-          </>
+          <span
+            className={styles.icon}
+            style={{ "--svg": `url(${icon})` } as React.CSSProperties}
+          ></span>
         ) : (
-          <span className={styles.icon}>{abbr}</span>
+          <span className={styles.abbr}>{abbr}</span>
         )}
         <Tooltip id={`pin-${id}`} ref={tooltipRef}>
           {link.name}
         </Tooltip>
-      </Link>
+      </Button>
     </li>
   );
 };
