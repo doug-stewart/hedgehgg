3507e6a370d0026b304cd132705f5fd90312a2bb
Doug Stewart <hi@dou.gg>
2026-07-20 21:32:44 -0700

    Removed unused import

--- src/hooks/useDialog.ts
+++ src/hooks/useDialog.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 
 export const useDialog = () => {
   const dialogRef = useRef<HTMLDialogElement>(null);
